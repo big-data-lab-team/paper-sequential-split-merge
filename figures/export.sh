10bd81e97524272d1d1f2dfa00041890ded185c4
Tristan Glatard <tristan.glatard@concordia.ca>
2017-06-11 21:08:41 -0400

    Figures for cluster reads

---
+++ figures/export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for name in d2-seeks d-seeks no-seek 
+do
+  echo "Exporting ${name}.svg"
+  inkscape -D -z --file=figures/${name}.svg --export-pdf=figures/${name}.pdf --export-latex
+  sed s,${name}\.pdf,figures/${name}\.pdf,g figures/${name}.pdf_tex > figures/${name}.pdf_tex.new
+  \mv figures/${name}.pdf_tex.new figures/${name}.pdf_tex
+done
