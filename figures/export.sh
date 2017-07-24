0d3d7fe454e378e2a2a9a7daf96a50d422cd93bf
Tristan Glatard <tristan.glatard@concordia.ca>
2017-07-24 16:42:03 -0400

    Fixed figure 2 (again)

--- figures/export.sh
+++ figures/export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for name in buffer-cluster-reads case1-a case1-b case1-c case2-a case2-b case3
+for name in buffer-cluster-reads case1-a case2-a case3
 do
   echo "Exporting ${name}.svg"
   inkscape -D -z --file=figures/${name}.svg --export-pdf=figures/${name}.pdf --export-latex
