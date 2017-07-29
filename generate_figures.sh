471dbf5d610a172ad9fd1c504f2383ea4bb5d3e3
YongpingGao <gaoyongping1990@gmail.com>
2017-07-29 09:48:27 -0400

    remove all average files and pdfs of figures generated by scripts

--- generate_figures.sh
+++ generate_figures.sh
@@ -4,17 +4,10 @@ set -e
 set -u
 
 figures/svg/export_svg_files.sh
-scripts/blocks-slices/createboxplot.gnplt
 scripts/model/model.gnplt
 scripts/mreads/number_of_seeks.gnuplot
-scripts/mreads/generate_avg_var_data.py
-scripts/mreads/compare_total_time.gnuplot
 scripts/mreads/compare_breakdown_errbar.gnuplot
-scripts/mwrites/generate_avg_var_data.py
-scripts/mwrites/compare_total_time.gnuplot
 scripts/mwrites/compare_breakdown_errbar.gnuplot
-
-
-
-
+#crash
+#scripts/blocks-slices/createboxplot.gnplt
 
