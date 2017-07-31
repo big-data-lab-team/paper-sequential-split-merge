be2d338b65dfacba938911a68751642089edbbfe
YongpingGao <gaoyongping1990@gmail.com>
2017-07-31 16:20:55 -0400

    add mreads-case figures

--- figures/svg/export_svg_files.sh
+++ figures/svg/export_svg_files.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for name in buffer case1-a case2-a case3 incomplete-rows overlap
+for name in buffer case1-a case2-a case3 incomplete-rows overlap mreads-case1 \
+        mreads-case2 mreads-case3 mreads-case4 mreads-case5
 do
   #echo "Exporting ${name}.svg"
   inkscape -D -z --file=figures/svg/${name}.svg --export-pdf=figures/svg/${name}.pdf --export-latex
