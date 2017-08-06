f7d2a992e6b6035a53a2b14bb971f4e4ebab28d5
YongpingGao <gaoyongping1990@gmail.com>
2017-08-06 19:58:15 -0400

    minor fix

--- scripts/experiment/generate_total_avg_var_data.py
+++ scripts/experiment/generate_total_avg_var_data.py
@@ -107,8 +107,15 @@ def generate(input_file_mreads, input_file_creads, input_file_buff_slices, outpu
 
     mem = ["0.6", "3", "6", "9", "12", "16"]
     with open(output_file, 'w+') as f:
-        f.write("mem \"Naive blocks\" NBR \"Naive slices\" NSR \"Cluster reads\" CRR \"Multiple reads\" MRR \"Buffered slices \" BSR")
-        f.write('\n')
+        if "merge" in output_file:
+            f.write("mem \"Naive blocks\" NBR \"Naive slices\" NSR \"Cluster reads\" CRR \"Multiple reads\" MRR \"Buffered slices \" BSR")
+            f.write('\n')
+        elif "split" in output_file:
+            f.write("mem \"Naive blocks\" NBR \"Naive slices\" NSR \"Cluster writes\" CRR \"Multiple writes\" MRR \"Buffered slices \" BSR")
+            f.write('\n')
+        else:
+            raise ValueError("output file should contains \"split\" or \"merge\"")
+
         for i in range(0,6):
             f.write(
                 "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}"
@@ -116,14 +123,17 @@ def generate(input_file_mreads, input_file_creads, input_file_buff_slices, outpu
                             ,mr_total[i], mr_total_err[i], bs_total[i], bs_total_err[i]))
             f.write('\n')
 
+
+
 def main():
     output_file = "./data/total-merge-time-ssd.dat"
     generate(input_file_mreads_ssd, input_file_creads_ssd, input_file_buff_slices_ssd, output_file=output_file)
     output_file = "./data/total-merge-time-hdd.dat"
     generate(input_file_mreads_hdd, input_file_creads_hdd, input_file_buff_slices_hdd, output_file=output_file)
-    output_file = "./data/total-split-time-ssd.dat"
-    # generate(input_file_mwrites_ssd, input_file_cwrites_ssd, input_file_buff_slices_split_ssd, output_file=output_file)
-    output_file = "./data/total-split-time-hdd.dat"
-    # generate(input_file_mwrites_hdd, input_file_cwrites_hdd, input_file_buff_slices_split_hdd, output_file=output_file)
+    #crash - add split data in cwrites/buff-slices
+    #output_file = "./data/total-split-time-ssd.dat"
+    #generate(input_file_mwrites_ssd, input_file_cwrites_ssd, input_file_buff_slices_split_ssd, output_file=output_file)
+    #output_file = "./data/total-split-time-hdd.dat"
+    #generate(input_file_mwrites_hdd, input_file_cwrites_hdd, input_file_buff_slices_split_hdd, output_file=output_file)
 if __name__ == '__main__':
     main()
