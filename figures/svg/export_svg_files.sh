61560f70e7198b6ececed1c2da3c354c58a6e274
Valerie Hayot-Sasson <valeriehayot@gmail.com>
2017-11-09 21:14:09 -0500

    fixed figures to be column-major

--- figures/svg/export_svg_files.sh
+++ figures/svg/export_svg_files.sh
@@ -4,7 +4,7 @@ for name in buffer case1-a case2-a case3 incomplete-rows overlap mreads-case1 \
         mreads-case2 mreads-case3 mreads-case4 mreads-case5 Notations
 do
   #echo "Exporting ${name}.svg"
-  inkscape -D -z --file=figures/svg/${name}.svg --export-pdf=figures/svg/${name}.pdf --export-latex
-  sed s,${name}\.pdf,figures/svg/${name}\.pdf,g figures/svg/${name}.pdf_tex > figures/svg/${name}.pdf_tex.new
-  \mv figures/svg/${name}.pdf_tex.new figures/svg/${name}.pdf_tex
+  inkscape -D -z --file=$PWD/figures/svg/${name}.svg --export-pdf=$PWD/figures/svg/${name}.pdf --export-latex
+  sed s,${name}\.pdf,$PWD/figures/svg/${name}\.pdf,g $PWD/figures/svg/${name}.pdf_tex > $PWD/figures/svg/${name}.pdf_tex.new
+  \mv $PWD/figures/svg/${name}.pdf_tex.new $PWD/figures/svg/${name}.pdf_tex
 done
